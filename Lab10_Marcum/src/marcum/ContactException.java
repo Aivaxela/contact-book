e8507027e665b7dd774e2b0093c262be55520452
Riley Marcum <rmarcum13@gmail.com>
2025-03-26 21:17:09 -0400

    complete lab10

---
+++ Lab10_Marcum/src/marcum/ContactException.java
@@ -0,0 +1,12 @@
+// Project:             Lab10
+// Class:               ITCS1820 Java Programming I
+// Date:                3/26/2025
+// Author:              Marcum
+// Description:         Exception Handling
+package marcum;
+
+public class ContactException extends Exception {
+    public ContactException(String message) {
+        super(message);
+    }
+}
