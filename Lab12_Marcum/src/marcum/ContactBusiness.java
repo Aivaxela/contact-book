a7aebd5862ca0087cbbf3df3e4686c92a339722b
Aivaxela <rmarcum13@gmail.com>
2025-04-04 21:39:14 -0400

    update lab11 to lab12

---
+++ Lab12_Marcum/src/marcum/ContactBusiness.java
@@ -0,0 +1,29 @@
+// Project:             lab12
+// Class:               ITCS1820 Java Programming I
+// Date:                3/26/2025
+// Author:              Marcum
+// Description:         Business subclass of Contact
+package marcum;
+
+public class ContactBusiness extends Contact {
+    private String company;
+
+    public ContactBusiness(
+            String name,
+            String email,
+            String phone,
+            String street,
+            String city,
+            String state,
+            String zip,
+            ContactType type,
+            String company) throws ContactException {
+        super(name, email, phone, street, city, state, zip, type);
+        this.company = company;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", " + company;
+    }
+}
